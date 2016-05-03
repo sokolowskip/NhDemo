b2aba45b14a1af8f1e16c9c58ecf3d451f878f86
Paweł Sokołowski <pawel.sokolowski@outlook.com>
2016-05-03 17:22:45 +0200

    Założenie projektu.
    Został założony nowy projekt i dołączono do niego bibliotękę
    wspierającą tesy jednostkowe. Został napisany pierwszy test
    weryfikujący możliwość uruchamiania testów.

---
+++ NhDemo/XUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace NhDemo
+{
+    public class XUnitTests
+    {
+        [Fact]
+        public void AddTwoIntegersTest()
+        {
+            (2+2).ShouldBe(4);
+        }
+    }
+}
